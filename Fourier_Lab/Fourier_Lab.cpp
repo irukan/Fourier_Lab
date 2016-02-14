0dcc957fc058ab4bbeed66a8950873915325d62b
kayama <kayama@kayama-Mac.local>
2016-02-14 18:43:12 +0900

    First commit

---
+++ Fourier_Lab/Fourier_Lab.cpp
@@ -0,0 +1,15 @@
+//
+//  Fourier_Lab.cpp
+//  Fourier_Lab
+//
+//  Created by kayama on 2016/02/14.
+//  Copyright © 2016年 kayama. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
